d60f3ef4a5ae23b76020c38ea819d57f33c21590
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2017-03-19 21:16:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 
+import sys
+
+from accounts.main_menu import MainMenu
+
+
+class LoginMessage:
+
+    def __init__(self):
+        pass
+
+    def login(self):
+        count = 0
+        with open("DB/credentials.txt", 'r') as f:
+            data = [x.strip().split(':') for x in f.readlines()]
+            print data
+            l = len(data)
+        while count<3:
+            user_name = input("Enter user Name: ")
+            password = input("Enter password: ")
+            if [user_name,password] in data[0:l]:
+                print("SUCCESSFULLY LOGGED INTO BMS")
+                o = MainMenu
+                o.main_menu()
+            else:
+                count += 1
+                print("User name and password doesnt match.")
+        else:
+            print 'Please contact for Administrative Assistance'
+            sys.exit()
